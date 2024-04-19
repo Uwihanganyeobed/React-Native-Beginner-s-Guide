8eccd7ce039e7f5130b99077d313d9b81092d392
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-04-19 10:12:26 +0200

    flex-shrink

--- App.js
+++ App.js
@@ -24,14 +24,14 @@ export default function App() {
   return (
     // <View style={{backgroundColor: 'plum', flex: 1}}></View>
     <View style={styles.container}>
-    <Box style={{backgroundColor: 'red',}}>Box 1</Box>
-    <Box style={{backgroundColor: 'orange'}}>Box 2</Box>
-    <Box style={{backgroundColor: 'yellow'}}>Box 3</Box>
+    <Box style={{backgroundColor: 'red',flexShrink: 1}}>Box 1 shrink</Box>
+    <Box style={{backgroundColor: 'orange', flexShrink: 2}}>Box 2 shrink</Box>
+    {/* <Box style={{backgroundColor: 'yellow'}}>Box 3</Box>
     <Box style={{backgroundColor: 'green'}}>Box 4</Box>
     <Box style={{backgroundColor: 'blue'}}>Box 5</Box>
     <Box style={{backgroundColor: 'indigo'}}>Box 6</Box>
     <Box style={{backgroundColor: 'violet'}}>Box 7</Box>
-    <Box style={{backgroundColor: 'orangered'}}>Box 8</Box>
+    <Box style={{backgroundColor: 'orangered'}}>Box 8</Box> */}
     </View>
     // <StyleSheetAPI/>
   );
@@ -40,16 +40,17 @@ const styles=StyleSheet.create({
   container: {
     // flex: 1,
     height: 300,
-    flexWrap: 'wrap',
+    width: 300,
+    // flexWrap: 'wrap',
     // rowGap: 20,
     // columnGap: 30,
-    gap: 20,
+    // gap: 20,
     // alignContent: 'space-around' ,
     marginTop: 30,
     borderWidth: 6,
-    // flexDirection: 'row',
+    flexDirection: 'row',
     // justifyContent: "flex-end",
-    // alignItems: 'baseline',
+    alignItems: 'flex-start',
     // borderColor: 'lightblue'
   }
 })
