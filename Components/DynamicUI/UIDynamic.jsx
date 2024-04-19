ed226a65c925d0b3df71ae02e9c83b9275ed126c
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-04-19 15:48:02 +0200

    UI Drawback

--- Components/DynamicUI/UIDynamic.jsx
+++ Components/DynamicUI/UIDynamic.jsx
@@ -1,15 +1,32 @@
 import { View, Text, StyleSheet,Dimensions } from "react-native";
+import { useState,useEffect } from "react";
 export default function UIDynamic(){
+   const [dimension, setDimension]=useState({
+      window: Dimensions.get('window')
+   })
+   useEffect(()=>{
+      const subscription=Dimensions.addEventListener("change",({window})=>{
+         setDimension({window});
+      });
+      return ()=>subscription?.remove();
+   });
+   const {window}=dimension;
+   const windowHeight=window.height;
+   const windowWidth=window.width;
    return(
       <View style={styles.container}>
-         <View style={styles.box}>
-            <Text style={styles.text}>Welcome</Text>
+         <View style={[styles.box,
+{               height: windowWidth>500? '70%': '90%',
+               width: windowHeight>600? "60%": '90%',}
+         ]}>
+            <Text style={{fontSize: windowWidth>500 ? 50:24 }}>Welcome</Text>
          </View>
       </View>
    )
 }
-const windowWidth=Dimensions.get("window").width;
-const windowHeight=Dimensions.get("window").height;
+// const windowWidth=Dimensions.get("window").width;
+// const windowHeight=Dimensions.get("window").height;
+// console.log({windowHeight,windowWidth});
 const styles=StyleSheet.create({
    container: {
       flex: 1,
@@ -19,14 +36,14 @@ const styles=StyleSheet.create({
       marginTop: 200
    },
    box:{
-      height: windowWidth>500? '70%': '90%',
-      width: windowHeight>600? "60%": '90%',
+      // height: windowWidth>500? '70%': '90%',
+      // width: windowHeight>600? "60%": '90%',
       backgroundColor: 'lightblue',
       alignItems: 'center',
       justifyContent: 'center'
    },
-   text:{
-      fontSize: windowWidth>500? 50:24
-   }
+   // text:{
+   //    fontSize: windowWidth>500? 50:24
+   // }
 })
 
